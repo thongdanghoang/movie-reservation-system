a8bf88827f03fab13833fa5c9d7cb684257cc192
Thong DANG HOANG <info@thongdanghoang.id.vn>
2026-02-18 06:51:14 +0000

    refactor: remove hold tracking columns from seats migration

--- services/reservation-service/src/main/resources/db/migration/V4__add_hold_tracking_to_seats.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE seats ADD COLUMN held_at TIMESTAMP NULL;
-ALTER TABLE seats ADD COLUMN held_by VARCHAR(255) NULL;
-ALTER TABLE seats ADD COLUMN reservation_id UUID NULL;
-
-CREATE INDEX idx_seats_held_at ON seats(held_at) WHERE held_at IS NOT NULL;
-CREATE INDEX idx_seats_reservation_id ON seats(reservation_id) WHERE reservation_id IS NOT NULL;
